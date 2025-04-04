8e7e92c92ced50cc6e7ee43ef5350535f04add73
Kabir Khan <kkhan@redhat.com>
2025-04-04 09:52:39 +0100

    Inject the config in the lib

--- finish/lib-jar/src/main/java/io/openliberty/guides/multimodules/lib/LibBean.java
+++ finish/lib-jar/src/main/java/io/openliberty/guides/multimodules/lib/LibBean.java
@@ -1,14 +1,19 @@
 package io.openliberty.guides.multimodules.lib;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 
 @ApplicationScoped
 public class LibBean {
+
+    @Inject
+    Config config;
+
     public Config getConfig() {
         System.out.println("LibBean: " + this.getClass().getClassLoader());
-        return ConfigProvider.getConfig();
+        return config;
     }
 }
